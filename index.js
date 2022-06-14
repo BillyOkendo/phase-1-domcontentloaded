7c65880c39a70752807a640eb3f57a154cf5c99f
Billy Otieno Okendo <billyotieno7@gmail.com>
2022-06-14 11:00:44 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let Line1 = document.getElementById("text");
+    Line1.textContent = "This is really cool!"
+  });
+  
+  
